02e5a44171c3dfa37f911cd787bfffaca1c41581
黄璇 <qd98xuan@sina.com>
2022-09-19 11:19:14 +0800

    修改列表

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'dart:ffi';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,19 +37,50 @@ class WeiboMsgListPage extends StatefulWidget {
 
 class _WeiboMsgListPageState extends State<WeiboMsgListPage> {
   int itemCount = 1;
+  var dataList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return RefreshIndicator(
-        child: ListView.builder(itemBuilder: (BuildContext context, int index) {
-          return const Padding(
-            padding: EdgeInsets.all(5),
-            child: Text(""),
-          );
-        },itemCount: itemCount,),
+        child: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: EdgeInsets.all(5),
+              child: Text(dataList[index].keyword),
+            );
+          },
+          itemCount: dataList.length,
+        ),
         onRefresh: () async {
-          setState(() {
-            itemCount =  20;
-          });
+          getData();
         });
   }
+
+  void getData() async {
+    var url = "http://coderutil.com/api/resou/v1/weibo?size=10";
+    try{
+      var result = await Dio().get(url);
+      if (jsonDecode(result.data)["code"] == 200) {
+        var list = json.decode(result.data)["data"];
+        setState(() {
+          dataList.addAll(list);
+        });
+      } else {}
+    }catch(e){
+      print(e.toString());
+    }
+  }
+}
+
+class HotData {
+  int rank = 0;
+  String keyword = "";
+  String url = "";
+  int hotValue = 0;
 }
